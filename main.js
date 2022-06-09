fcd148e58c859fe6148dfaca13c469b413a788ee
Lux <92374048+lucas38460@users.noreply.github.com>
2022-06-09 13:50:28 +0200

    départ du projet

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import './style.css'
+const pierre = document.getElementById('pierre');
+const feuille = document.getElementById('feuille');
+const ciseaux = document.getElementById('ciseaux');
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
