aab1b964153ebff579b67f31a8efa0271cfe7f17
Lux <92374048+lucas38460@users.noreply.github.com>
2022-06-13 09:01:37 +0200

    fin de l'exo en cours

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 const Btn = document.querySelectorAll("button");
+const p = document.getElementById('choice');
+let resultat = "";
+let timer = 0;
+
+let timerVal = setInterval(() => {
+  if (timer === 6) {
+    console.log(resultat);
+    clearInterval(timerVal);
+  } else {
+    timer++;
+  }
+}, 1000);
 
 for (let i = 0; i < Btn.length; i++) {
-    const element = Btn[i];
-    let resultat = "";
-    element.addEventListener('click', ()=>{
-      const robot = Btn[Math.floor(Math.random() * Btn.length)];
-      const joueur = element;
+  const element = Btn[i];
+  const joueur = element.textContent;
+  element.addEventListener("click", () => {
+    p.innerHTML = `${element.textContent}`;
+    const robot = Btn[Math.floor(Math.random() * Btn.length)].textContent;
 
-      if(joueur === robot) {
-        resultat = "Égalité";
-      } else if () {
-        resultat = "Vous avez gagné";
-      } else  {
-        resultat = "Vous avez perdu"
-      }
-    });
+    if (joueur === robot) {
+      resultat = "Egalité";
+    } else if (
+      (joueur === "pierre" && robot === "ciseaux") ||
+      (joueur === "feuille" && robot === "pierre") ||
+      (joueur === "ciseaux" && robot === "feuille")
+    ) {
+      resultat = "Gagné";
+    } else {
+      resultat = "Perdu";
+    }
+  });
 }
